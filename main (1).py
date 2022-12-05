fbdcb88cdf95789c0a819a391db0d96fdfff3440
Jatin Parashar <78733719+jatinparashar333@users.noreply.github.com>
2022-12-05 09:10:50 +0530

    Check Whether a Number is Even or Odd in Python

---
+++ main (1).py
@@ -0,0 +1,19 @@
+Check Whether a Number is Even or Odd in Python
+Given an integer input the objective is to write a Python code to Check Whether a Number is Even or Odd. To do so the main idea is to divide the number by 2 and check if it’s divisible or not. It’s an Even number is it’s perfectly divisible by 2 or an Odd number otherwise.
+
+
+
+
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+num=input()
+num=int(num)
+if num%2==0:
+    print('even')
+else:
+    print('odd')
