ec68ed38ff29d2b3cc806f3b5c598d63e418fc81
Jatin Parashar <78733719+jatinparashar333@users.noreply.github.com>
2022-12-08 09:04:47 +0530

    Find the Greatest of the Three Numbers

---
+++ main (5).py
@@ -0,0 +1,20 @@
+'''
+
+                            Online Python Compiler.
+                Code, Compile, Run and Debug python program online.
+Write your code in this editor and press "Run" button to execute it.
+
+'''
+
+a=int(input())
+b=int(input())
+c=int(input())
+if a>b:
+    if a>c:
+        print(a)
+    else:
+        print(c)
+elif b>c:
+    print(b)
+else:
+    print(c)
